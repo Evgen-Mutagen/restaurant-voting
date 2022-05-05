1563d4b6ac744b5df6d74f653eb9440b1899b7f3
Evgen-Mutagen <jeka190@mail.ru>
2022-05-05 23:10:01 +0300

    fix repository and pom

--- src/main/java/ru/javaops/topjava2/repository/VoteRepository.java
+++ src/main/java/ru/javaops/topjava2/repository/VoteRepository.java
@@ -1,12 +1,13 @@
 package ru.javaops.topjava2.repository;
 
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import ru.javaops.topjava2.model.Vote;
 
 import java.time.LocalDate;
 import java.util.List;
-
+@Repository
 public interface VoteRepository extends BaseRepository<Vote> {
     @Override
     int delete(int id);
